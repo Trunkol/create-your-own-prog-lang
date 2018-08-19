5fe24ac94a9783185467bf6b39c09ed5f0cf54d4
Emerson Costa <costtasemerson@gmail.com>
2018-08-19 11:51:24 -0300

    [Add] add runtime and define context for each class and node

---
+++ runtime/method.rb
@@ -0,0 +1,17 @@
+class AwesomeMethod
+    def initialize(params, body)
+        @params = params
+        @body = body
+    end
+
+    def call(receiver, arguments)
+       context = Context.new(receiver)
+       
+       @params.each_with_index do |param, index|
+        context.locals[params] = arguments[index]
+       end
+
+       
+       @body.eval(context)
+    end
+end
